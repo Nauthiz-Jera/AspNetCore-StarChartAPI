ae18f972555e53aec84054cc9e0d926df3d8df11
edKimEnd <ed.kim@endurance.com>
2019-07-30 11:24:37 -0400

    step-two

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestiaObject> CelestiaObjects { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
